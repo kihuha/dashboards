4587a1448202468737b00af7a40a89ad055be7e4
kihuha <njihiadarius@gmail.com>
2021-01-23 11:36:42 +0300

    Completed eleven

--- src/dashboards/eleven.tsx
+++ src/dashboards/eleven.tsx
@@ -2,10 +2,19 @@ import { useEffect, useRef } from "react"
 import { RouteComponentProps } from "@reach/router"
 import styled from "styled-components"
 import * as d3 from "d3"
+import { BsPlus, BsChevronDown } from "react-icons/bs"
 
 // COMPONENTS
 import Layout from "../components/layout"
 import Badge from "../components/badge"
+
+// IMAGES
+import One from "../img/eleven/one.png"
+import Two from "../img/eleven/two.png"
+import Three from "../img/eleven/three.png"
+import Four from "../img/eleven/four.png"
+import Five from "../img/eleven/five.png"
+import Six from "../img/eleven/six.png"
 
 const Top = styled.div`
   display: grid;
@@ -39,6 +48,34 @@ const CardContentWrapper = styled.div`
   align-items: flex-start;
   justify-content: space-between;
 `
+
+const CardFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 3.2rem;
+`
+
+const Avatar = styled.img`
+  margin-right: 0.5rem;
+`
+
+const AvatarPlus = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 100%;
+  background: #4339f2;
+  color: white;
+`
+
+const CardIcon = styled(AvatarPlus)`
+  background: #ffe5d3;
+  color: #ff3a29;
+`
+
 const Bottom = styled.div`
   margin-top: 2rem;
   margin-bottom: 3rem;
@@ -51,6 +88,22 @@ const BottomHeader = styled.div`
 `
 
 const Left = styled.div``
+
+const ChartButton = styled.button`
+  display: flex;
+  align-items: center;
+  margin-left: 10rem;
+  padding: 0.5rem;
+  font-size: 1.4rem;
+  border: none;
+  outline: none;
+  background: none;
+  font-family: inherit;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.05);
+  }
+`
 
 const Right = styled.div``
 
@@ -86,6 +139,7 @@ interface IContent {
     color: string
     label: string
   }[]
+  images: any[]
 }
 
 const Eleven = (props: RouteComponentProps) => {
@@ -260,6 +314,7 @@ const Eleven = (props: RouteComponentProps) => {
           color: "#E2FBD7",
         },
       ],
+      images: [One, Two, Three],
     },
     {
       title: "Space The Final Frontier",
@@ -276,6 +331,7 @@ const Eleven = (props: RouteComponentProps) => {
           color: "#CCF8FE",
         },
       ],
+      images: [Four],
     },
     {
       title: "Moon Gazing",
@@ -292,6 +348,7 @@ const Eleven = (props: RouteComponentProps) => {
           color: "#FFE5D3",
         },
       ],
+      images: [Five, Six],
     },
   ]
   return (
@@ -310,6 +367,34 @@ const Eleven = (props: RouteComponentProps) => {
               <CardAmount>${content.amount}</CardAmount>
             </CardContentWrapper>
             <CardBody>{content.body}</CardBody>
+            <CardFooter>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                {content.images.map((image) => (
+                  <Avatar src={image} alt="" />
+                ))}
+                <AvatarPlus>
+                  <BsPlus size={20} />
+                </AvatarPlus>
+              </div>
+              <div>
+                <CardIcon>
+                  <svg
+                    width="18"
+                    height="14"
+                    viewBox="0 0 18 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M10.9409 13.667C10.8851 13.667 10.8276 13.6645 10.7701 13.6586C10.0367 13.5861 9.46175 13.0686 9.30341 12.3395L7.06758 2.01281L4.76508 7.33115C4.63258 7.63615 4.33175 7.83365 4.00008 7.83365H1.50008C1.03925 7.83365 0.666748 7.46031 0.666748 7.00031C0.666748 6.53948 1.03925 6.16698 1.50008 6.16698H3.45341L5.54175 1.34281C5.83758 0.662813 6.48925 0.26698 7.23008 0.341147C7.96341 0.413647 8.53841 0.931147 8.69675 1.66031L10.9326 11.987L13.2351 6.66865C13.3676 6.36365 13.6676 6.16698 14.0001 6.16698H16.5001C16.9609 6.16698 17.3334 6.53948 17.3334 7.00031C17.3334 7.46031 16.9609 7.83365 16.5001 7.83365H14.5467L12.4584 12.657C12.1859 13.2845 11.6017 13.667 10.9409 13.667"
+                      fill="#FF3A29"
+                    />
+                  </svg>
+                </CardIcon>
+              </div>
+            </CardFooter>
           </Card>
         ))}
       </Top>
@@ -317,18 +402,19 @@ const Eleven = (props: RouteComponentProps) => {
         <BottomHeader>
           <Left>
             <h2 style={{ fontSize: "2.2rem" }}>Portfolio</h2>
-            <div style={{ display: "flex", marginTop: "1rem" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "1rem",
+              }}
+            >
               <p style={{ fontSize: "1.4rem", color: "rgba(0,0,0,0.4)" }}>
                 king_ernser@sigurd.biz
               </p>
-              <div
-                style={{
-                  marginLeft: "10rem",
-                  fontSize: "1.4rem",
-                }}
-              >
-                One Year
-              </div>
+              <ChartButton>
+                One Year <BsChevronDown style={{ marginLeft: "0.5rem" }} />
+              </ChartButton>
             </div>
           </Left>
           <Right>
